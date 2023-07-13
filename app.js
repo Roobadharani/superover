f4fa658bb91093608cbb9772e4e15dedead17eb4
Roobadharani <127882154+Roobadharani@users.noreply.github.com>
2023-07-13 15:43:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+
+let cskruns = ["_","_","_","_","_","_"];
+let miruns = ["_","_","_","_","_","_"];
+
+let cskscore = 0;
+let miscore = 0;
+let ballcounter = 0;
+
+function strikeBall(){
+    ballcounter++;
+    let run = Math.floor(Math.random()*5)*2;
+    if(run ==8)
+    {
+        run = 'w';
+    } 
+    //csk
+    if(ballcounter<=6)
+    {
+        if(run!='w')
+        {
+            cskscore = cskscore+run;
+            document.getElementById("cskscore").textContent = cskscore;
+        }
+        cskruns[ballcounter-1] = run;
+        document.getElementById("cskruns").textContent = cskruns;
+
+    }
+    else if(ballcounter<=12)
+    {
+        if(run!='w')
+        {
+            miscore = miscore+run;
+            document.getElementById("miscore").textContent = miscore;
+        }
+        miruns[ballcounter-7] = run;
+        document.getElementById("miruns").textContent = miruns;
+
+        if(miscore==cskscore)
+        {
+            ballcounter =12;
+        }
+    }
+    
+    if(ballcounter>=12)
+    {
+        document.getElementById("bat").disabled = true;
+        
+        let maxscore=Math.max(cskscore,miscore);
+        if(cskscore == maxscore)
+        {
+            document.getElementById("csk").classList.add('winner');
+        }
+        else if(cskscore<=miscore)
+        {
+            document.getElementById("mi").classList.add('winner');
+        }
+    }
+}
